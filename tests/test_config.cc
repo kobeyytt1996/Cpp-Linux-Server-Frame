e0541fb4d78f6ad3bc946da3996f6e53773a5e6b
Yuan Yuan <13718503098@163.com>
2022-03-04 09:16:39 +0800

    能够正常使用yaml第三方库

--- tests/test_config.cc
+++ tests/test_config.cc
@@ -1,13 +1,22 @@
 #include "../yuan/config.h"
 #include "../yuan/log.h"
+#include <yaml-cpp/yaml.h>
 
 // 通过Config获得基础的配置项
 yuan::ConfigVar<int>::ptr g_int_value_config =
     yuan::Config::Lookup("system.port", (int)8080, "system.port");
 
+void test_yaml() {
+    YAML::Node root = YAML::LoadFile("/home/yuan/workspace/yuan/bin/conf/log.yml");
+
+    YUAN_LOG_INFO(YUAN_GET_ROOT_LOGGER()) << root;
+}
+
 int main() {
     YUAN_LOG_INFO(YUAN_GET_ROOT_LOGGER()) << g_int_value_config->getValue();
     YUAN_LOG_INFO(YUAN_GET_ROOT_LOGGER()) << g_int_value_config->toString();
+
+    test_yaml();
 
     return 0;
 }
