35d4a562add3eb295013fed56aabc2af4408e4d0
Yuan Yuan <13718503098@163.com>
2022-03-25 09:01:17 +0800

    增加socket Address的封装

---
+++ yuan/address.cc
@@ -0,0 +1,160 @@
+#include "address.h"
+
+#include <algorithm>
+#include <sstream>
+
+namespace yuan {
+
+/**
+ * Address的实现
+ */
+int Address::getFamily() const {
+    return getAddr()->sa_family;
+}
+
+std::string Address::toString() const {
+    std::stringstream ss;
+    print(ss);
+    return ss.str();
+}
+
+bool Address::operator<(const Address &rhs) const {
+    socklen_t min_len = std::min(getAddrLen(), rhs.getAddrLen());
+    int result = memcmp(getAddr(), rhs.getAddr(), min_len);
+
+    if (result < 0) {
+        return true;
+    } else if (result > 0) {
+        return false;
+    } else if (getAddrLen() < rhs.getAddrLen()) {
+        return true;
+    }
+    return false;
+}
+
+bool Address::operator==(const Address &rhs) const {
+    return getAddrLen() == rhs.getAddrLen() 
+        && memcmp(getAddr(), rhs.getAddr(), getAddrLen()) == 0;
+}
+
+bool Address::operator!=(const Address &rhs) const {
+    return !(*this == rhs);
+}
+
+/**
+ * IPv4Address的方法实现
+ */
+IPv4Address::IPv4Address(uint32_t address, uint32_t port) {
+
+}
+
+const sockaddr *IPv4Address::getAddr() const {
+
+}
+
+socklen_t IPv4Address::getAddrLen() const {
+
+}
+
+std::ostream &IPv4Address::print(std::ostream &os) const {
+
+}
+
+IPAddress::ptr IPv4Address::broadcastAddress(uint32_t prefix_len) {
+
+}
+
+IPAddress::ptr IPv4Address::networkAddress(uint32_t prefix_len) {
+
+}
+
+IPAddress::ptr IPv4Address::subnetMask(uint32_t prefix_len) {
+
+}
+
+uint32_t IPv4Address::getPort() const {
+
+}
+
+void IPv4Address::setPort(uint32_t port) {
+
+}
+
+/**
+ * IPv6Address的方法实现
+ */
+IPv6Address::IPv6Address(uint32_t address, uint32_t port) {
+
+}
+
+const sockaddr *IPv6Address::getAddr() const {
+
+}
+
+socklen_t IPv6Address::getAddrLen() const {
+
+}
+
+std::ostream &IPv6Address::print(std::ostream &os) const {
+
+}
+
+IPAddress::ptr IPv6Address::broadcastAddress(uint32_t prefix_len) {
+
+}
+
+IPAddress::ptr IPv6Address::networkAddress(uint32_t prefix_len) {
+
+}
+
+IPAddress::ptr IPv6Address::subnetMask(uint32_t prefix_len) {
+
+}
+
+uint32_t IPv6Address::getPort() const {
+
+}
+
+void IPv6Address::setPort(uint32_t port) {
+
+}
+
+/**
+ * UnixAddress
+ */
+UnixAddress::UnixAddress(const std::string &path) {
+
+}
+
+const sockaddr *UnixAddress::getAddr() const {
+
+}
+
+socklen_t UnixAddress::getAddrLen() const {
+
+}
+
+std::ostream &UnixAddress::print(std::ostream &os) const {
+
+}
+
+/**
+ * UnknownAddress
+ */
+UnknownAddress::UnknownAddress() {
+
+}
+
+const sockaddr *UnknownAddress::getAddr() const {
+
+}
+
+socklen_t UnknownAddress::getAddrLen() const {
+
+}
+
+std::ostream &UnknownAddress::print(std::ostream &os) const {
+
+}
+
+}
---
+++ yuan/address.h
@@ -0,0 +1,121 @@
+#ifndef __YUAN_ADDRESS_H__
+#define __YUAN_ADDRESS_H__
+
+/**
+ * 封装三种socket address的类，IPv4，IPv6和UNIXAddress（靠进程间文件传输，不走协议栈，效率更高）
+ * 
+ */
+
+#include <arpa/inet.h>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/un.h>
+
+namespace yuan {
+
+// 要被各种地址子类继承的基类
+class Address {
+public:
+    typedef std::shared_ptr<Address> ptr;
+
+    virtual ~Address() {}
+    int getFamily() const;
+
+    virtual const sockaddr *getAddr() const = 0;
+    virtual socklen_t getAddrLen() const = 0;
+
+    virtual std::ostream &print(std::ostream &os) const = 0;
+    std::string toString() const;
+
+    bool operator<(const Address &rhs) const;
+    bool operator==(const Address &rhs) const;
+    bool operator!=(const Address &rhs) const;
+
+};
+
+class IPAddress : public Address {
+public:
+    typedef std::shared_ptr<IPAddress> ptr;
+
+    virtual IPAddress::ptr broadcastAddress(uint32_t prefix_len) = 0;
+    virtual IPAddress::ptr networkAddress(uint32_t prefix_len) = 0;
+    virtual IPAddress::ptr subnetMask(uint32_t prefix_len) = 0;
+
+    // 索然端口号目前最大是65535.但未来没准会扩大，所以给的范围更大
+    virtual uint32_t getPort() const = 0;
+    virtual void setPort(uint32_t port) = 0;
+};
+
+class IPv4Address : public IPAddress {
+public:
+    typedef std::shared_ptr<IPv4Address> ptr;
+    IPv4Address(uint32_t address = INADDR_ANY, uint32_t port = 0);
+
+    const sockaddr *getAddr() const override;
+    socklen_t getAddrLen() const override;
+    std::ostream &print(std::ostream &os) const override;
+
+    IPAddress::ptr broadcastAddress(uint32_t prefix_len) override;
+    IPAddress::ptr networkAddress(uint32_t prefix_len) override;
+    IPAddress::ptr subnetMask(uint32_t prefix_len) override;
+
+    uint32_t getPort() const override;
+    void setPort(uint32_t port) override;
+
+private:
+    sockaddr_in m_addr;
+};
+
+class IPv6Address : public IPAddress {
+public:
+    typedef std::shared_ptr<IPv6Address> ptr;
+    IPv6Address(uint32_t address = INADDR_ANY, uint32_t port = 0);
+
+    const sockaddr *getAddr() const override;
+    socklen_t getAddrLen() const override;
+    std::ostream &print(std::ostream &os) const override;
+
+    IPAddress::ptr broadcastAddress(uint32_t prefix_len) override;
+    IPAddress::ptr networkAddress(uint32_t prefix_len) override;
+    IPAddress::ptr subnetMask(uint32_t prefix_len) override;
+
+    uint32_t getPort() const override;
+    void setPort(uint32_t port) override;
+
+private:
+    sockaddr_in6 m_addr;
+};
+
+class UnixAddress : public Address {
+public:
+    typedef std::shared_ptr<UnixAddress> ptr;
+    UnixAddress(const std::string &path);
+
+    const sockaddr *getAddr() const override;
+    socklen_t getAddrLen() const override;
+    std::ostream &print(std::ostream &os) const override;
+
+private:
+    sockaddr_un m_addr;
+    socklen_t m_length;
+};
+
+class UnknownAddress : public Address {
+public:
+    typedef std::shared_ptr<UnknownAddress> ptr;
+    UnknownAddress();
+
+    const sockaddr *getAddr() const override;
+    socklen_t getAddrLen() const override;
+    std::ostream &print(std::ostream &os) const override;
+
+private:
+    sockaddr m_addr;
+};
+
+}
+
+#endif
