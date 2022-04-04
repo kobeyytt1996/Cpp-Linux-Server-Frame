0ee220bf6fdc590485716bc8a9a7f0f0dea1f350
Yuan Yuan <13718503098@163.com>
2022-04-04 23:55:16 +0800

    增加TcpServer的bind实现

--- yuan/tcp_server.cc
+++ yuan/tcp_server.cc
@@ -1,53 +1,37 @@
-#ifndef __YUAN_TCP_SERVER_H__
-#define __YUAN_TCP_SERVER_H__
-
-#include <functional>
-#include <memory>
-#include <vector>
-
-#include "address.h"
-#include "iomanager.h"
-#include "socket.h"
+#include "config.h"
+#include "log.h"
+#include "tcp_server.h"
 
 namespace yuan {
 
-class TcpServer : public std::enable_shared_from_this<TcpServer> {
-public:
-    typedef std::shared_ptr<TcpServer> ptr;
-    TcpServer(IOManager *worker = IOManager::GetThis());
-    // 可以作为基类让不同的协议进行扩展
-    virtual ~TcpServer() {}
+static Logger::ptr g_system_logger = YUAN_GET_LOGGER("system");
 
-    virtual bool bind(yuan::Address::ptr addr);
-    // 支持多个地址的bind
-    virtual bool bind(std::vector<yuan::Address::ptr> addrs);
-    // 启动服务器
-    virtual bool start();
-    // 停止服务器
-    virtual bool stop();
+static yuan::ConfigVar<uint64_t>::ptr g_tcp_server_read_timeout = 
+    yuan::Config::Lookup("tcp_server.read_timeout", (uint64_t)60 * 1000 * 2, "tcp_server read_timeout");
 
-    uint64_t getReadTimeout() const { return m_readTimeout; }
-    const std::string getName() const { return m_name; }
-    bool isStop() const { return m_isStop; } 
-    
-    void setReadTimeout(uint64_t readTimeout) { m_readTimeout = readTimeout; }
-    void setName(const std::string &name) { m_name = name; }
-protected:
-    // 每当accept到一个socket，就调用该方法进行处理
-    virtual void handleClient(Socket::ptr client);
-private:
-    // 存储listen的socket。会支持多协议，可以同时监听多地址。比如，有两个网卡，一个内网一个外网
-    std::vector<Socket::ptr> m_socks;
-    // 当作线程池来使用。socket accept之后，要交给线程池处理
-    IOManager *m_worker;
-    // 要防止无效的或者恶意的连接。除了在报文格式上要检查，这里也要增加超时时间，防止fd等资源被浪费
-    uint64_t m_readTimeout;
-    // 可能会有多个server，根据名字来在log的时候做区分
-    std::string m_name;
-    // Server是否已停止
-    bool m_isStop;
-};
+TcpServer::TcpServer(IOManager *worker)
+    : m_worker(worker)
+    , m_readTimeout(g_tcp_server_read_timeout->getValue())
+// 命名格式为：功能/版本号。比如框架进行了升级，那么可以用版本号进行标识
+    , m_name("yuan/1.0.0")
+    , m_isStop(false) {}
+
+bool TcpServer::bind(yuan::Address::ptr addr) {
+    std::vector<yuan::Address::ptr> addrs(1, addr);
+    return bind(addrs);
+}
+
+bool TcpServer::bind(const std::vector<yuan::Address::ptr> &addrs) {
+    // 传入的多个地址可能只有部分可以bind。比如有一些端口号已被占用
+    bool ret = true;
+}
+
+bool TcpServer::start() {
 
 }
 
-#endif
+bool TcpServer::stop() {
+
+}
+
+}
---
+++ yuan/tcp_server.h
@@ -0,0 +1,54 @@
+#ifndef __YUAN_TCP_SERVER_H__
+#define __YUAN_TCP_SERVER_H__
+
+#include <functional>
+#include <memory>
+#include <vector>
+
+#include "address.h"
+#include "iomanager.h"
+#include "socket.h"
+
+namespace yuan {
+
+class TcpServer : public std::enable_shared_from_this<TcpServer> {
+public:
+    typedef std::shared_ptr<TcpServer> ptr;
+    TcpServer(IOManager *worker = IOManager::GetThis());
+    // 可以作为基类让不同的协议进行扩展
+    virtual ~TcpServer() {}
+
+    // 无论是哪种IP协议，IPv4或IPv6，都可以支持bind
+    virtual bool bind(yuan::Address::ptr addr);
+    // 支持多个地址的bind。也是上面的bind的具体实现
+    virtual bool bind(const std::vector<yuan::Address::ptr> &addrs);
+    // 启动服务器
+    virtual bool start();
+    // 停止服务器
+    virtual bool stop();
+
+    uint64_t getReadTimeout() const { return m_readTimeout; }
+    const std::string getName() const { return m_name; }
+    bool isStop() const { return m_isStop; } 
+    
+    void setReadTimeout(uint64_t readTimeout) { m_readTimeout = readTimeout; }
+    void setName(const std::string &name) { m_name = name; }
+protected:
+    // 每当accept到一个socket，就调用该方法进行处理
+    virtual void handleClient(Socket::ptr client);
+private:
+    // 存储listen的socket。会支持多协议，可以同时监听多地址。比如，有两个网卡，一个内网一个外网
+    std::vector<Socket::ptr> m_socks;
+    // 当作线程池来使用。socket accept之后，要交给线程池处理
+    IOManager *m_worker;
+    // 要防止无效的或者恶意的连接。除了在报文格式上要检查，这里也要增加超时时间，防止fd等资源被浪费
+    uint64_t m_readTimeout;
+    // 可能会有多个server，根据名字来在log的时候做区分
+    std::string m_name;
+    // Server是否已停止
+    bool m_isStop;
+};
+
+}
+
+#endif
